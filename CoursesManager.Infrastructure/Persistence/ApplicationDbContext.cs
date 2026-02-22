0cb3deca41f5b4294287b039b524fe842e45518c
TimLearnToCode <tim.matthiessen@mail.com>
2026-02-22 18:58:07 +0100

    Add database context

---
+++ CoursesManager.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using CoursesManager.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoursesManager.Infrastructure.Persistence;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<CourseEntity> Courses { get; set; }
+    public DbSet<CourseSessionEntity> CourseSessions { get; set; }
+    public DbSet<LocationEntity> Locations { get; set; }
+    public DbSet<TeacherEntity> Teachers { get; set; }
+    public DbSet<ParticipantEntity> Participants { get; set; }
+    public DbSet<RegistrationEntity> Registrations { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Hämtar automatiskt alla konfigurationsfiler som bestämmer hur tabellerna ska se ut i databasen.
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
